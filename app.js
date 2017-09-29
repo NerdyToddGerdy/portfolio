cd58633f7ddaf9323b9c31d0701fb8465710e0ee
Todd Gerdy <toadimous@gmail.com>
2017-09-29 12:38:05 -0400

    added terminal to bio page

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ $( ()=> {
   // sections
   const $bio = $('#bio');
   const $contact = $('#contact');
-
+  const $terminal = $('.terminal');
 
   // functions
   const closeAllPages = () => {
+    $terminal.hide();
     $bio.hide();
     $contact.hide();
   };
@@ -19,7 +20,16 @@ $( ()=> {
   // listeners
   $bioButton.click( () =>{
     closeAllPages();
-    $bio.show();
+    $terminal.show();
+    // $('#animate-bio').addClass('animate')
+    $terminal.append('<h1 class="animate">$This is a test<h1>');
+    $('.animate').one("webkitTranstionEnd animationend otranstionsend oTransitionEnd msTransitionEnd transitionend",
+      function(e) {
+        // console.log('its done');
+        $bio.show();
+        $terminal.html('')
+      }
+    )
   });
 
   $contactButton.click( ()=> {
